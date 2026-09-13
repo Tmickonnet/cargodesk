c7dad66096d91e3e4fb0970284c86e8eddfd9bfe
AYODELE TAIWO MICHEAL <52764672+Tmickonnet@users.noreply.github.com>
2026-09-13 02:45:24 +0100

    Place App.jsx in correct source directory

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>CargoDesk Global</h1>
+      <p>Logistics Operations Platform</p>
+    </div>
+  );
+}
+
+export default App;
